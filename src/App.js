6b3e1603037c260c229c7184cdd7861fd3592b50
Renner <rennermccreath@gmail.com>
2021-08-03 22:44:05 -0400

    initial commit and setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import SignupPage from './pages/SignupPage/SignupPage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import UserPage from './pages/UserPage/UserPage';
+import AboutPage from './pages/AboutPage/AboutPage';
+import ThingsPage from './pages/AboutPage/ThingsPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      
+      user: userService.getUser()
+    };
+  }
+
+  handleLogout = () => {
+    userService.logout();
+    this.setState({ user: null });
+  }
+
+  handleSignupOrLogin = () => {
+    this.setState({user: userService.getUser()});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className='header-footer'>
+          <Link to='/'>RetroChat.exe</Link>
+        </header>
+        <Switch>
+          <Route exact path='/' render={() =>
+          <HomePage />
+          }/>
+          <Route exact path='/user' render={() =>
+          <UserPage />
+          }/>
+          <Route exact path='/about' render={() =>
+            <AboutPage />
+          }/>
+          <Route exact path='/signup' render={() =>
+            <SignupPage />
+          }/>
+          <Route exact path='/login' render={() =>
+            <LoginPage />
+          }/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
